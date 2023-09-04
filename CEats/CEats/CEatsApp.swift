4fcc24d2ca791b07af36f4cf6d398951e55d3e71
Time5607 <115935304+Time5607@users.noreply.github.com>
2023-09-04 14:25:25 +0900

    9/4 워크스페이스 생성 머지

---
+++ CEats/CEats/CEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CEatsApp.swift
+//  CEats
+//
+//  Created by 박범수 on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct CEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
