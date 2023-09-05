dea5ac0f7ae1e1610094b7ac87c429591b47be11
Sangwoo Byeon <tkddn0518@naver.com>
2023-09-05 10:42:03 +0900

    [Add] lightgray 색상 추가, Extension으로 Color로 추가하여 사용가능하도록 추가

---
+++ CEats/CEats/Extension/Color+.swift
@@ -0,0 +1,13 @@
+//
+//  Color+.swift
+//  CEats
+//
+//  Created by 변상우 on 2023/09/05.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let lightgray = Color("lightgray")
+}
