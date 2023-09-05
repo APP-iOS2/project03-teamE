bdbef32873b810248330a3f77df7742ce0d39a59
yuhaeun-la <exok1and1m@gmail.com>
2023-09-05 13:13:48 +0900

    [Feature]Zstack으로 위에 알림띄움

--- CEats/CEats/Views/Home/Map/MapHomeView.swift
+++ CEats/CEats/Views/Home/Map/MapHomeView.swift
@@ -17,15 +17,33 @@ struct MapHomeView: View {
                 Rectangle()
                     .foregroundColor(.veryLightGray) //이거 색을 다른걸로 하든 섹션으로 분류하든 해야겟으요
                     .frame(height: 5)
-                Button {
-                    
+                NavigationLink {
+                    NowMapView(isSelectedPlace: .constant(""), selectedPlaceLat: .constant(0), selectedPlaceLong: .constant(0))
                 } label: {
                     Label("현재 위치로 주소찾기", systemImage: "location.north")
                 }
                 .padding(.top,20)
                 .buttonStyle(.borderedProminent)
-
                 
+                ScrollView{
+                    ForEach(0..<5, id:\.self){ content in
+                        HStack{
+                            Image(systemName: "bell")
+                            VStack(alignment: .leading){
+                                Text("잠실 더 힐츠 아파트")
+                                    .font(.system(size:15))
+                                Text("서울시 잠실 잠실대로 1023 1302-606")
+                                    .font(.footnote)
+                                    .foregroundColor(.secondary)
+                            }
+                            Spacer()
+                        }
+                        .padding(30)
+                        Spacer()
+                        Divider()
+
+                    }
+                }
             }
         }
         .toolbar {
