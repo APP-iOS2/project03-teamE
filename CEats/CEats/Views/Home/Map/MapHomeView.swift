c10804c2bca80c9526a3bf26dc7acaa7ddc2ef56
Sangwoo Byeon <tkddn0518@naver.com>
2023-09-05 17:03:28 +0900

    [Fix] MapHomeView Binding 값 뷰마다 넘겨주기

--- CEats/CEats/Views/Home/Map/MapHomeView.swift
+++ CEats/CEats/Views/Home/Map/MapHomeView.swift
@@ -8,52 +8,68 @@
 import SwiftUI
 
 struct MapHomeView: View {
+    
     @Environment(\.dismiss) private var dismiss
+    
+    @Binding var isOpenMapSheet: Bool
 
     var body: some View {
-        NavigationStack{
-            VStack{
-                MapSearchView()
+        NavigationStack {
+            VStack {
+                NavigationLink {
+                    MapSearchView(isOpenMapSheet: $isOpenMapSheet)
+                } label: {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.lightgray)
+                            .padding(.trailing, 10)
+                        Text("도로명, 건물명 또는 지번으로 검색")
+                            .foregroundColor(.lightgray)
+                        
+                        Spacer()
+                    }
+                    .padding(.leading, 30)
+                }
+                .padding(.top, 30)
+                .padding(.bottom, 10)
+                
                 Rectangle()
-                    .foregroundColor(.veryLightGray) //이거 색을 다른걸로 하든 섹션으로 분류하든 해야겟으요
+                    .foregroundColor(.veryLightGray)
                     .frame(height: 5)
                 NavigationLink {
-                    NowMapView(isSelectedPlace: .constant(""), selectedPlaceLat: .constant(0), selectedPlaceLong: .constant(0))
+                    NowMapView(isOpenMapSheet: $isOpenMapSheet, isSelectedPlace: .constant(""), selectedPlaceLat: .constant(0), selectedPlaceLong: .constant(0))
                 } label: {
                     Label("현재 위치로 주소찾기", systemImage: "location.north")
+                        .frame(width: .screenWidth * 0.8, height: 60)
+                        .background(.clear)
+                        .foregroundColor(.black)
+                        .border(Color.gray, width: 1.5)
+                        .cornerRadius(3)
                 }
-                .padding(.top,20)
-                .buttonStyle(.borderedProminent)
+                .padding(.top, 20)
                 
-                ScrollView{
-                    ForEach(0..<5, id:\.self){ content in
-                        HStack{
-                            Image(systemName: "bell")
-                            VStack(alignment: .leading){
-                                Text("잠실 더 힐츠 아파트")
-                                    .font(.system(size:15))
-                                Text("서울시 잠실 잠실대로 1023 1302-606")
-                                    .font(.footnote)
-                                    .foregroundColor(.secondary)
-                            }
-                            Spacer()
-                        }
-                        .padding(30)
-                        Spacer()
-                        Divider()
-
-                    }
-                }
+                Spacer()
             }
+            .navigationTitle("주소 설정")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: backButton)
         }
-}
-        
+    }
     
-        
+    var backButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "xmark")
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color.black)
+        }
+    }
 }
 
 struct MapHomeView_Previews: PreviewProvider {
     static var previews: some View {
-        MapHomeView()
+        MapHomeView(isOpenMapSheet: .constant(true))
     }
 }
