7dd6092bfce9f288e502c9d570b961a92ad69354
Nnn <jaehyung1299@naver.com>
2023-09-06 13:19:19 +0900

    유저 모델 수정중

--- CEats/CEats/Models/User.swift
+++ CEats/CEats/Models/User.swift
@@ -12,10 +12,14 @@ struct User: Identifiable {
     var username: String
     var email: String
     var phoneNumber: String
-//    var homeAddress: location이랑 똑같은 타입일거같음
+    var latitude: Double//위도
+    var longitude: Double //경도
     var favoriteRestaurant: [Restaurant]
     var orderHistory : [Order] //
     var foodCart: [Food] //장바구니
 }
 
 
+extension User {
+    static let sampleData = User(id: "a1", username: "김멋사", email: "abc@gmail.com", phoneNumber: "010-1234-5678", latitude: 32.44, longitude: 55.22, favoriteRestaurant: [Restaurant(id: "b2", password: "1234", name: "홍길동", reviews: [Review(writer: "김멋사", score: 4.0, contents: "최고의 맛이었어요 ㅠㅠ")], deliveryFee: 3000, minimumPrice: 14000, menus: [Food(name: "김치찌개", price: 8000, isRecommend: true, foodCategory: "김치찌개", description: "멋쟁이 김치찌개 인기메뉴")], mainImage: [""], foodType: [.korean], foodCategory: ["식사","사이드","주류"], latitude: 32.44, longitude: 55.22)], orderHistory: [Order(id: "c3", orderer: <#T##User#>, restaurantName: <#T##Restaurant#>, orderedMenu: <#T##[Food]#>, deliveryTime: <#T##Int#>, totalFee: <#T##Int#>)], foodCart: <#T##[Food]#>)
+}
