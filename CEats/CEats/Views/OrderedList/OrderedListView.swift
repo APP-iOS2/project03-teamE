a89e87b903ee318005c4dedb3c7ddbecbc9c042b
Nnn <jaehyung1299@naver.com>
2023-09-06 14:17:29 +0900

    MyPage, OrderedList 뷰 수정

--- CEats/CEats/Views/OrderedList/OrderedListView.swift
+++ CEats/CEats/Views/OrderedList/OrderedListView.swift
@@ -8,8 +8,93 @@
 import SwiftUI
 
 struct OrderedListView: View {
+    
+    @State var search: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("과거 주문 내역")
+                .font(.title3)
+            NavigationView {
+                List {
+                    ZStack(alignment: .leading) {
+                        VStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 350, height: 250)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.gray, lineWidth: 1)
+                                )
+                                .foregroundColor(.white)
+                                .padding(2)
+                            
+                        }
+                        VStack(alignment: .leading) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    
+                                    Text("멋쟁이 김치찌개")
+                                        .font(.title2)
+                                        .bold()
+                                    Text("2023-xx-xx 오전 xx:xx")
+                                        .foregroundColor(.gray)
+                                    Text("배달 완료")
+                                        .padding(.top, 5)
+                                }
+                                .padding(.trailing, 25)
+                                Image("kimchiSoup")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                            }
+                            Text("1     김치찌개")
+                                .padding(.top, 10)
+                            Text("합계: 15,000원")
+                                .padding(.top, 10)
+                        }
+                        .padding(.leading, 30)
+                        
+                    }
+                    ZStack(alignment: .leading) {
+                        VStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 350, height: 250)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.gray, lineWidth: 1)
+                                )
+                                .foregroundColor(.white)
+                                .padding(2)
+                            
+                        }
+                        VStack(alignment: .leading) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    
+                                    Text("멋쟁이 피자")
+                                        .font(.title2)
+                                        .bold()
+                                    Text("2023-xx-xx 오전 xx:xx")
+                                        .foregroundColor(.gray)
+                                    Text("배달 중")
+                                        .padding(.top, 5)
+                                }
+                                .padding(.trailing, 25)
+                                Image("pizza")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                            }
+                            Text("3     페퍼로니 피자")
+                                .padding(.top, 10)
+                            Text("합계: 23,000원")
+                                .padding(.top, 10)
+                        }
+                        .padding(.leading, 30)
+                        
+                    }
+                }
+                .listStyle(.plain)
+            }
+        }
     }
 }
 
