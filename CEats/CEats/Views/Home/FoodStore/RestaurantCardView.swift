4c0f2ea43926841c5d89a6aac1ebda3ef61bc547
jangyeohoon <someday0307@naver.com>
2023-09-06 15:20:45 +0900

    Update RestaurantCardView.swift

--- CEats/CEats/Views/Home/FoodStore/RestaurantCardView.swift
+++ CEats/CEats/Views/Home/FoodStore/RestaurantCardView.swift
@@ -21,51 +21,55 @@ struct RestaurantCardView: View {
     var body: some View {
         NavigationStack {
             // FoodType으로 먼저 분류 -> 분류된 목록에서 ForEach로 반복
-            ForEach(restaurantsStore.filterFoodTypes(selectedFoodType)) { store in
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 380, height: 300)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                        .foregroundColor(.white)
-                        .padding(2)
-                        .shadow(radius: 3)
-                    NavigationLink {
-//                        RestaurantView(restaurant: $store)
-                    } label: {
-                        VStack {
-                            ZStack {
-                                AsyncImage(url: URL(string: "https://www.jungle.co.kr/image/90e4ffc149968a50c80cac37"))
-                                    .frame(width: 300, height: 200)
-                                    .cornerRadius(10)
-                                
-                                Image(systemName: heartImage)
-                                    .background(
-                                        Circle()
-                                            .foregroundColor(.white)
-                                            .frame(width: 40, height: 40)
-                                    )
-                                    .opacity(0.7)
-                                    .foregroundColor(.red)
-                                    .font(.title)
-                                    .offset(x: 140, y: -60)
-                                    .onTapGesture {
-                                        isFavorited.toggle()
-                                    }
-                            }
-                            HStack {
+            ForEach(restaurantsStore.filterFoodTypes(selectedFoodType)){ store in
+                NavigationLink {
+                    //                        RestaurantView(restaurant: $store)
+                } label: {
+                    VStack {
+                        ZStack {
+                            AsyncImage(url: URL(string: "https://www.jungle.co.kr/image/90e4ffc149968a50c80cac37"))
+                                .frame(width: .screenWidth * 0.92, height: .screenHeight * 0.23)
+                                .cornerRadius(10)
+                            
+                            Image(systemName: heartImage)
+                                .font(.system(size:20))
+                                .background(
+                                    Circle()
+                                        .foregroundColor(.white)
+                                        .frame(width: 30,height: 30)
+                                        .opacity(0.6)
+                                )
+                                .foregroundColor(.red)
+                                .font(.title)
+                                .offset(x: .screenWidth/2.6, y: -(.screenHeight/13))
+                                .onTapGesture {
+                                    isFavorited.toggle()
+                                }
+                        }
+                        
+                        VStack{
+                            HStack{
                                 Text("\(store.name)")
-                                    .font(.title2)
+                                    .bold()
+                                    .padding(.bottom,0.1)
                                 Spacer()
-                                Text("최소 금액: \(store.minimumPrice)원")
+                                Text("35~45 분") //배달시간 프로퍼티가 업서요~ 필요할거 같아요 이게 오더에 있어요.
+                                    .foregroundColor(.gray)
+                                    .font(.subheadline)
                             }
-                            HStack {
-                                Text("⭐️ 평점 : \(store.scoreMessage)")
+                            HStack{
+                                Image(systemName: "star.fill")
+                                    .foregroundColor(.yellow)
+                                Text("\(store.scoreMessage)")
+                                Text("(\(store.reviews.count))")
+                                Text("1.5km")
+                                Text("-")
                                 Text("배달비 : \(store.deliveryFee)원")
                                 Spacer()
                             }
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                            
                         }
                         .padding()
                         .foregroundColor(.black)
@@ -75,6 +79,7 @@ struct RestaurantCardView: View {
         }
     }
 }
+
 
 struct RestaurantCardView_Previews: PreviewProvider {
     static var previews: some View {
