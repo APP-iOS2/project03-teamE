29c924bb05a716e95b0b1b9e6eea16ae2ff16815
Time5607 <115935304+Time5607@users.noreply.github.com>
2023-09-07 17:22:19 +0900

    [creat] CartAppealView, CartpaymentView

---
+++ CEats/CEats/Views/Home/Cart/CartAppealView.swift
@@ -0,0 +1,20 @@
+//
+//  CartAppealView.swift
+//  CEats
+//
+//  Created by 박범수 on 2023/09/07.
+//
+
+import SwiftUI
+
+struct CartAppealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CartAppealView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartAppealView()
+    }
+}
---
+++ CEats/CEats/Views/Home/Cart/CartPaymentView.swift
@@ -0,0 +1,20 @@
+//
+//  CartPaymentView.swift
+//  CEats
+//
+//  Created by 박범수 on 2023/09/07.
+//
+
+import SwiftUI
+
+struct CartPaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CartPaymentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartPaymentView()
+    }
+}
