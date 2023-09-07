1ce567bdde8a1e32ffcc833ba345659df45e3076
Time5607 <115935304+Time5607@users.noreply.github.com>
2023-09-07 14:47:01 +0900

    [creat] CartDeliveryView, CartMenuView

---
+++ CEats/CEats/Views/Home/Cart/CartDeliveryView.swift
@@ -0,0 +1,20 @@
+//
+//  CartDeliveryView.swift
+//  CEats
+//
+//  Created by 박범수 on 2023/09/07.
+//
+
+import SwiftUI
+
+struct CartDeliveryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CartDeliveryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartDeliveryView()
+    }
+}
---
+++ CEats/CEats/Views/Home/Cart/CartMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  CartMenuView.swift
+//  CEats
+//
+//  Created by 박범수 on 2023/09/07.
+//
+
+import SwiftUI
+
+struct CartMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CartMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartMenuView()
+    }
+}
