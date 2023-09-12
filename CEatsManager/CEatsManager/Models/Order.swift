fd1d630e7f2243343a3023cd410c4ce3f8836444
Time5607 <115935304+Time5607@users.noreply.github.com>
2023-09-12 16:08:58 +0900

    [Creat] Manager Models. Fb Code

---
+++ CEatsManager/CEatsManager/Models/Order.swift
@@ -0,0 +1,59 @@
+//
+//  Order.swift
+//  CEatsManager
+//
+//  Created by 박범수 on 2023/09/12.
+//
+
+import Foundation
+
+
+struct Order: Identifiable, CEatsIdentifiable, Codable {
+    var id: String
+    var orderer: String //이거 문제있음
+    var restaurant : Restaurant //
+    var orderedMenu: [Restaurant.Food]
+    var rtrRequest: String?
+    var deliveryRequest: String?
+    var orderStatus: OrderStatus = .waiting
+    var deliveryTime: String {
+        return restaurant.deliveryTime
+    }
+    var totalFee: Int {
+        let totalFoodFee = orderedMenu.map({ $0.price }).reduce(0) { $0 + $1 }
+        return totalFoodFee + restaurant.deliveryFee
+    }
+    var orderedAt: Double = Date().timeIntervalSince1970
+    var orderDate: String {
+        let dateOrderedAt: Date = Date(timeIntervalSince1970: orderedAt)
+        
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_kr")
+        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        dateFormatter.dateFormat = "MM월dd일 HH:mm"
+        return dateFormatter.string(from: dateOrderedAt)
+    }
+    
+    enum OrderStatus: Codable {
+        case waiting, canceled, accepted
+        
+        var toString: String {
+            switch self {
+            case .waiting:
+                return "준비중"
+            case .canceled:
+                return "주문 취소됨"
+            case .accepted:
+                return "주문 완료됨"
+            }
+        }
+    }
+}
+
+
+#if DEBUG
+extension Order {
+    static let sampleData: Self = .init(id: UUID().uuidString, orderer: "김민지", restaurant: .sampleData, orderedMenu: [Restaurant.Food(name: "김치찌개", price: 10000, isRecommend: true, foodCategory: "김치찌개", description: "멋쟁이 김치찌개 인기메뉴"), Restaurant.Food(name: "공기밥", price: 1500, isRecommend: false, foodCategory: "밥", description: "")], orderStatus: .canceled)
+}
+#endif
+
---
+++ CEatsManager/CEatsManager/Models/User.swift
@@ -0,0 +1,47 @@
+//
+//  User.swift
+//  CEatsManager
+//
+//  Created by 박범수 on 2023/09/12.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable, CEatsIdentifiable, Equatable {
+    static func == (lhs: User, rhs: User) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    var id: String
+    var username: String
+    var email: String
+    var phoneNumber: String
+    var userAddress: String
+    var favoriteRestaurant: [Restaurant] // 즐겨찾기
+    var orderHistory: [Order] //
+    var foodCart: Cart? //장바구니
+    var cEatsMoney: Int = 100000
+    var latitude: Double//위도
+    var longitude: Double //경도
+    var reviews: [Review]
+    var coupons: [Coupon]
+    
+    struct Cart: Codable {
+        var restaurant: Restaurant
+        var restaurantName: String {
+            return restaurant.name
+        }
+        var cart: [Restaurant.Food] // [filter] 푸드의 이름이 같은 것 count 해서 숫자 사용
+        var fee: Int {
+            let totalFoodFee = cart.map({ $0.price * $0.foodCount }).reduce(0) { $0 + $1 }
+            return totalFoodFee + restaurant.deliveryFee
+        }
+        
+    }
+}
+
+#if DEBUG
+extension User {
+    static var sampleData: Self = User(id: "1234", username: "김민지", email: "newJean@naver.com", phoneNumber: "010-0000-0000", userAddress: "노원구 공롱동 12-34", favoriteRestaurant: [], orderHistory: [Order.sampleData], foodCart: Cart(restaurant: Restaurant.sampleData, cart: [Restaurant.Food.sampleData]) , latitude: 0 ,longitude: 0, reviews: .sampleData, coupons: [.sampleData])
+}
+#endif
