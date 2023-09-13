a6a45f4423bbee451d3a03f951a0997ef73f02d2
Time5607 <115935304+Time5607@users.noreply.github.com>
2023-09-13 15:12:01 +0900

    [fix] plist build problem

--- CEatsManager/CEatsManager/FireBase/FireBaseManager.swift
+++ CEatsManager/CEatsManager/FireBase/FireBaseManager.swift
@@ -1,8 +1,8 @@
-
-  FireBaseManager.swift
-  CEatsManager
-
-  Created by 박범수 on 2023/09/12.
+//
+//  FireBaseManager.swift
+//  CEatsManager
+//
+//  Created by 박범수 on 2023/09/12.
 
 
 import Foundation
