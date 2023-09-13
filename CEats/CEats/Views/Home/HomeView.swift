7fab70952d5c161fe55b670fae8a8cf0118c39ac
yuhaeun-la <exok1and1m@gmail.com>
2023-09-13 14:26:18 +0900

    [Fix]AddCartViewError

--- CEats/CEats/Views/Home/HomeView.swift
+++ CEats/CEats/Views/Home/HomeView.swift
@@ -44,6 +44,7 @@ struct HomeView: View {
             userViewModel.fetchUser {
                 print("온어피어적용됨~")
             }
+            
         }
         .fullScreenCover(isPresented: $isOpenMapSheet) {
             MapHomeView(isOpenMapSheet: $isOpenMapSheet)
