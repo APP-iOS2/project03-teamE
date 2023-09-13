ceafcd3c9930a0ffeae51a3270313acf16cb4e87
Bongjooheon <112856133+Bongjooheon@users.noreply.github.com>
2023-09-13 10:48:19 +0900

    [Fix] ReviewDetail, ReviewInfo 수정

--- CEatsManager/CEatsManager/Review/ReviewDetail.swift
+++ CEatsManager/CEatsManager/Review/ReviewDetail.swift
@@ -21,28 +21,29 @@ struct ReviewDetail: View {
         ScrollView {
             VStack(alignment: .leading) {
                 VStack(alignment: .leading) {
-                    HStack {
                         Text("가게 위치 : ")
+                            .font(.system(size: 20))
                             .bold()
                         // 위도 경도 받아와서 가게 주소
-                    }
                     Text("가게 전화번호 : \(restaurant.restaurantInfo.phoneNumberString)")
+                        .font(.system(size: 20))
                         .bold()
                 }
-                .font(.system(size: 30))
-                .padding(.bottom, 50)
+                .padding([.leading, .bottom], 20)
+                Divider()
                 
                 ForEach(restaurant.reviews) { review in
                     ReviewInfo(review: review)
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
-                        .padding(.bottom, 30)
+//                        .padding()
+//                        .background(
+//                            RoundedRectangle(cornerRadius: 10)
+//                                .stroke(Color.black, lineWidth: 1)
+//                                .frame(width:1000, height: 450)
+//                                .padding(.top, 10)
+//                        )
                 }
             }
-            .padding(20)
+//            .padding(20)
             .navigationTitle("\(restaurant.name)")
         }
     }
