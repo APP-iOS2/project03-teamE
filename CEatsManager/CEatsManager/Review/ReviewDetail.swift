9f496519caccf219679089f5d7ef4e052c579c92
jangyeohoon <someday0307@naver.com>
2023-09-13 10:21:52 +0900

    [FIX] ReviewDetail

--- CEatsManager/CEatsManager/Review/ReviewDetail.swift
+++ CEatsManager/CEatsManager/Review/ReviewDetail.swift
@@ -14,6 +14,7 @@ struct ReviewDetail: View {
     // MARK: - Properties
     @EnvironmentObject var restaurantViewModel: RestaurantViewModel
     @EnvironmentObject var userViewModel: UserViewModel
+    @State private var place = ""
     var restaurant: Restaurant
     
     // MARK: - Views
@@ -22,7 +23,7 @@ struct ReviewDetail: View {
             VStack(alignment: .leading) {
                 VStack(alignment: .leading) {
                     HStack {
-                        Text("가게 위치 : ")
+                        Text("가게 위치 : \(place)")
                             .bold()
                         // 위도 경도 받아와서 가게 주소
                     }
@@ -44,6 +45,30 @@ struct ReviewDetail: View {
             }
             .padding(20)
             .navigationTitle("\(restaurant.name)")
+            .onAppear {
+               convertLocationToAddress(location: CLLocation(latitude: restaurant.latitude, longitude: restaurant.longitude))
+            }
+        }
+    }
+    
+    // CLLocation의 위도, 경도 값을 주소값으로 변경해주는 함수
+    // 함수 호출 과정 중, MapKit의 애플 서버 연동 과정 중 에러가 발생할 경우가 있음
+    // 조금 기다리면 다시 정상 작동하지만, 빠르게 위도와 경도값이 바뀌는 경우 조금 시간이 걸림
+    func convertLocationToAddress(location: CLLocation) {
+
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if error != nil {
+                print("ERROR!!")
+                return
+            }
+            
+            guard let placemark = placemarks?.first else { return }
+            
+            place = "\(placemark.locality ?? "") \(placemark.name ?? "")"
+            
+            print("장소:\(place)")
         }
     }
 }
