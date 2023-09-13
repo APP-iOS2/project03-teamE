c57f565e0ed53a378fe612ed6815d759c433faa0
yuhaeun-la <exok1and1m@gmail.com>
2023-09-13 12:12:14 +0900

    [Fix] 가게 이미지 제대로 안뜨는거 수정했음
    어싱크 이미지 -> 그냥 이미지

--- CEats/CEats/Views/Home/FoodStore/RestaurantCardView.swift
+++ CEats/CEats/Views/Home/FoodStore/RestaurantCardView.swift
@@ -19,7 +19,7 @@ struct RestaurantCardView: View {
         NavigationStack {
             // FoodType으로 먼저 분류 -> 분류된 목록에서 ForEach로 반복
             ForEach(restaurantsStore.filterFoodTypes(selectedFoodType)){ store in
-                NavigationLink {
+                NavigationLink { //
                     RTRView(restaurant: store)
                 } label: {
                     VStack {
